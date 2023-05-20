e7cbe710b43e065b8c68e96f272a5f50fe276e14
Yoo Seungmin <dnjfht@gmail.com>
2023-05-21 02:21:56 +0900

    React Query - devtools

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import MainProducts from "./components/MainProducts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // react-query에서 client 가져오기
@@ -11,6 +12,8 @@ function App() {
     <QueryClientProvider client={queryClient}>
       {/* QueryClientProvider로 어플리케이션 감싸기 => 우산 씌워주기 */}
       <MainProducts />
+      <ReactQueryDevtools initialIsOpen={true} />
+      {/* 처음에 바로 개발툴이 나오게 하는 것을 뜻하는 true */}
     </QueryClientProvider>
   );
 }
