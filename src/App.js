2abb8e5dbc3d77a3ed36ae6f89d409852305b243
Yoo Seungmin <dnjfht@gmail.com>
2023-05-20 19:17:10 +0900

    react-query

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import MainProducts from "./components/MainProducts";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// react-query에서 client 가져오기
+const queryClient = new QueryClient();
+// client 초기화
+
 function App() {
-  return <MainProducts />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* QueryClientProvider로 어플리케이션 감싸기 => 우산 씌워주기 */}
+      <MainProducts />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
